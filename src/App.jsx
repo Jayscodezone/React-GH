24a4eb59356591eb46b540858f040876486df3ee
Janaya Cooper <janayacooper@janayas-mbp.mynetworksettings.com>
2025-01-06 18:07:32 -0500

    completed initalizing app and completing table and fixing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 
 import './App.css'
+import { useState } from 'react';
 import ContactList from './components/ContactList'
 
 export default function App(){
+ 
   return (
     <>
     <ContactList />
